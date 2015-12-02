1ee6072d92b0328cb3465f1330d3570f3a69bbb0
Nurha <nurha@projectcodex.co>
2015-12-02 15:55:09 +0200

    rank table

---
+++ database_tables/ranks.sql
@@ -0,0 +1,4 @@
+INSERT INTO ranks(rank_name, rank_type, latitude, longitude) VALUES('Cape Town Station', 'Official', -33.924706, 18,422787);
+INSERT INTO ranks(rank_name, rank_type, latitude, longitude) VALUES('Mithchells Plain Station', 'Official', -34.0145819, 18.3871117);
+INSERT INTO ranks(rank_name, rank_type, latitude, longitude) VALUES('Khayelitsha', 'Official', -34.0131259, 18.3857373);
+INSERT INTO ranks(rank_name, rank_type, latitude, longitude) VALUES('Bellville Station', 'Official', -33.8850214, 18.5570999);

ee364c74ec869be0aac776721d3e5d0504536009
We Dance Again <ayabonga@projectcodex.co>
2015-12-02 17:20:24 +0200

    Update getmethere_test.sql

--- getmethere_test.sql
+++ getmethere_test.sql
@@ -17,29 +17,29 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `getmethere_test`
+-- Database: "getmethere_test"
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `rank`
+-- Table structure for table "rank"
 --
 
-CREATE TABLE IF NOT EXISTS `rank` (
-  `rank_id` int(11) NOT NULL AUTO_INCREMENT,
-  `rank_name` char(100) DEFAULT NULL,
-  `rank_type` char(100) DEFAULT NULL,
-  `latitude` decimal(42,10) DEFAULT NULL,
-  `longitude` decimal(42,10) DEFAULT NULL,
-  PRIMARY KEY (`rank_id`)
+CREATE TABLE IF NOT EXISTS "rank" (
+  "rank_id" int(11) NOT NULL AUTO_INCREMENT,
+  "rank_name" char(100) DEFAULT NULL,
+  "rank_type" char(100) DEFAULT NULL,
+  "latitude" decimal(42,10) DEFAULT NULL,
+  "longitude" decimal(42,10) DEFAULT NULL,
+  PRIMARY KEY ("rank_id")
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
--- Dumping data for table `rank`
+-- Dumping data for table "rank"
 --
 
-INSERT INTO `rank` (`rank_id`, `rank_name`, `rank_type`, `latitude`, `longitude`) VALUES
+INSERT INTO "rank" ("rank_id", "rank_name", "rank_type", "latitude", "longitude") VALUES
 (1, 'Cape Town', 'official', 3.1457890000, 70.9856210000),
 (2, 'Bellville', 'official', 10.1457800000, -20.1447600000),
 (3, 'Khayelitsha', 'official', 10.2546980000, 40.2365800000);
@@ -47,24 +47,24 @@ INSERT INTO `rank` (`rank_id`, `rank_name`, `rank_type`, `latitude`, `longitude`
 -- --------------------------------------------------------
 
 --
--- Table structure for table `route`
+-- Table structure for table "route"
 --
 
-CREATE TABLE IF NOT EXISTS `route` (
-  `route_id` int(11) NOT NULL AUTO_INCREMENT,
-  `rank_id` int(42) DEFAULT NULL,
-  `route_name` char(100) DEFAULT NULL,
-  `price` decimal(42,2) DEFAULT NULL,
-  `destination_id` int(11) NOT NULL,
-  PRIMARY KEY (`route_id`),
-  KEY `rank_id` (`rank_id`)
+CREATE TABLE IF NOT EXISTS "route" (
+  "route_id" int(11) NOT NULL AUTO_INCREMENT,
+  "rank_id" int(42) DEFAULT NULL,
+  "route_name" char(100) DEFAULT NULL,
+  "price" decimal(42,2) DEFAULT NULL,
+  "destination_id" int(11) NOT NULL,
+  PRIMARY KEY ("route_id"),
+  KEY "rank_id" ("rank_id")
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
--- Dumping data for table `route`
+-- Dumping data for table "route"
 --
 
-INSERT INTO `route` (`route_id`, `rank_id`, `route_name`, `price`, `destination_id`) VALUES
+INSERT INTO "route" ("route_id", "rank_id", "route_name", "price", "destination_id") VALUES
 (1, 2, 'Bellville N2 Cape Town CBD', 16.50, 1),
 (3, 2, 'Bellville - Khayelitsha', 13.00, 3);
 
@@ -73,10 +73,10 @@ INSERT INTO `route` (`route_id`, `rank_id`, `route_name`, `price`, `destination_
 --
 
 --
--- Constraints for table `route`
+-- Constraints for table "route"
 --
-ALTER TABLE `route`
-  ADD CONSTRAINT `route_ibfk_1` FOREIGN KEY (`rank_id`) REFERENCES `rank` (`rank_id`);
+ALTER TABLE "route"
+  ADD CONSTRAINT "route_ibfk_1" FOREIGN KEY ("rank_id") REFERENCES "rank" ("rank_id");
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

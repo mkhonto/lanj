d2af3ffc3b914cca5486ba7bc160b596cddc0aac
We Dance Again <ayabonga@projectcodex.co>
2015-12-03 10:24:10 +0200

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,20 +1,22 @@
 DROP TABLE IF EXISTS `ranks`;
-CREATE TABLE `ranks` (
-    `rank_id` int NOT NULL auto_increment,
+CREATE TABLE `rank` (
+    `id` int NOT NULL auto_increment,
     `rank_name` char(100),
     `rank_type` char(100),
-    `latitude` decimal(42,10),
-    `longitude` decimal(42,10),
-    PRIMARY KEY(rank_id));
+    `latitude` decimal(10,7),
+    `longitude` decimal(10,7),
+    PRIMARY KEY(id),
+    CONSTRAINT uc_rank_name  UNIQUE (rank_name)
+);
 
 
 DROP TABLE IF EXISTS `routes`;
-CREATE TABLE `routes` (
-    `route_id` int NOT NULL auto_increment,
+CREATE TABLE `route` (
+    `id` int NOT NULL auto_increment,
     `rank_id` int(42),
-    `route_name` char(100),
-    `destination_id` char(100),
+    `destination_id` int(42),
     `price` decimal(42,2),
-    PRIMARY KEY(route_id),
-    FOREIGN KEY (rank_id) REFERENCES ranks(rank_id)
+    PRIMARY KEY(id),
+    FOREIGN KEY (rank_id) REFERENCES ranks(id),
+    FOREIGN KEY (destination_id)  REFERENCES ranks(id)
 );
